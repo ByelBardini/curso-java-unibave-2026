05689541fa7d6c990608ecdc7cd4769b9a29a113
KauaNamiki <kakanamiki@gmail.com>
2026-06-26 20:17:45 -0300

    terminando a classe Aluno

--- simulado/Aluno.java
+++ simulado/Aluno.java
@@ -12,6 +12,39 @@ public class Aluno {
         this.nota2 = nota2;
     }
 
+    public String getNome() {
+        return nome;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public double getNota1() {
+        return nota1;
+    }
+
+    public double getNota2() {
+        return nota2;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public void setNota1(double nota1) {
+        this.nota1 = nota1;
+    }
+
+    public void setNota2(double nota2) {
+        this.nota2 = nota2;
+    }
+
+
     public double getMedia() {
         return (nota1 + nota2) / 2;
     }
@@ -19,6 +52,12 @@ public class Aluno {
     public boolean aprovado() {
         return getMedia() >= 7;
     }
-
-    // getters e setters
+    @Override
+    public String toString() {
+        return "Nome: " + nome +
+                "\nMatrícula: " + matricula +
+                "\nNota 1: " + nota1 +
+                "\nNota 2: " + nota2 +
+                "\nMédia: " + getMedia();
+    }
 }
--- simulado/Main.java
+++ simulado/Main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
 public class Main {
     void main(){
+        Scanner sc = new Scanner(System.in);
 
+        int opcao;
+
+        do {
+
+            System.out.println("1 - Cadastrar");
+            System.out.println("2 - Listar");
+            System.out.println("3 - Buscar");
+            System.out.println("4 - Remover");
+            System.out.println("0 - Sair");
+
+            opcao = sc.nextInt();
+
+        } while(opcao != 0);
     }
 }
